c5f30ab5f5e17b091b36e23258dfbbb81d9b3d2c
Artem Chikin <achikin@apple.com>
2026-03-11 16:25:29 +0000

    [DNM] Experiment with default 'LiteralExpressions'

--- lib/AST/FeatureSet.cpp
+++ lib/AST/FeatureSet.cpp
@@ -419,7 +419,7 @@ static bool usesFeatureBuiltinConcurrencyStackNesting(Decl *decl) {
 }
 
 UNINTERESTING_FEATURE(CompileTimeValuesPreview)
-UNINTERESTING_FEATURE(LiteralExpressions)
+//UNINTERESTING_FEATURE(LiteralExpressions)
 UNINTERESTING_FEATURE(StrictMemorySafety)
 UNINTERESTING_FEATURE(LibraryEvolution)
 UNINTERESTING_FEATURE(SafeInteropWrappers)
